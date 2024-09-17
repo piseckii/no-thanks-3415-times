92252257bfceee17bf362863b1ef686d22877ea5
piseckii <pisetsky738@mail.ru>
2024-09-17 12:15:23 +0300

    Создан класс Deck, для него написаны методы __init__, __repr__, __eq__, save, load, draw_card, shuffle + тесты для всех кроме shuffle

--- src/deck.py
+++ src/deck.py
@@ -1,7 +1,36 @@
 from src.card import Card
+import random
 
 class Deck:
-    def __init_(self, cards: list[Card] | None = None):
+    def __init__(self, cards: list[Card] | None = None):
         if cards == None:
-            print('test')
+            cards = Card.all_cards
+        self.cards: list[Card] = cards
+        
+        
+    def __repr__(self):
+        """Вывод массива карт"""
+        return f'{self.cards}'
+    
+    def __eq__(self, other):
+        return self.cards == other.cards
+        
+      
+    def save(self) -> list[str]:
+        return [Card.save(card) for card in self.cards]
+    
+    
+    @classmethod
+    def load(cls, ls: list[str]):
+        """Convert list[str] to list[Card]"""
+        return cls(cards = [Card.load(txt) for txt in ls])
+
+
+    def draw_card(self):
+        """Берем карту из колоды и возвращаем ее."""
+        return self.cards.pop(0)
+
+
+    def shuffle(self):
+        random.shuffle(self.cards)
         
---
+++ test/test_deck.py
@@ -0,0 +1,61 @@
+import pytest
+from src.card import Card
+from src.deck import Deck
+
+
+def test_init():
+    cards = [Card(12), Card(15), Card(16)]
+    
+    d = Deck(cards)
+    
+    assert d.cards == cards
+
+
+def test_repr():
+    cards = [Card(12), Card(15), Card(16)]
+    
+    d = Deck(cards)
+    d1 = Deck([Card(9)])
+    
+    assert d.__repr__() == "[|12|, |15|, |16|]"
+    assert d1.__repr__() == "[|9|]"
+    assert Deck([Card(4)]).__repr__() == "[|4|]"
+
+
+def test_eq():
+    cards = [Card(12), Card(15), Card(16)]
+     
+    d = Deck(cards)
+    d1 = Deck(cards)
+    d2 = Deck([Card(12), Card(15), Card(16)])
+    d3 = Deck([Card(4), Card(5)])
+    
+    assert d == d1
+    assert d == d2
+    assert d != d3
+
+def test_save():
+     cards = [Card(12), Card(15), Card(16)]
+     
+     d = Deck(cards)
+     
+     assert d.save() == ['12', '15', '16']
+     assert Deck(cards).save() ==  ['12', '15', '16']
+     assert Deck([Card(3)]).save() == ['3']
+
+
+def test_load():
+    cards = [Card(12), Card(15), Card(16)]
+     
+    d = Deck(cards).save()
+    
+    assert Deck.load(d) == Deck(cards)
+    assert Deck.load(['12']) == Deck([Card(12)])
+
+def test_draw_card():
+    cards = [Card(12), Card(15), Card(16)]
+     
+    d = Deck(cards)
+    
+    assert d.draw_card() == Card(12)
+    assert d == Deck([Card(15), Card(16)])
