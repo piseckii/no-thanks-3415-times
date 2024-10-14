2124f4140e1472808627b0d82979ce60273f58bb
piseckii <pisetsky738@mail.ru>
2024-10-14 13:14:51 +0300

    Копипаст из уно: game_server, player_interaction, __init__, ai_player,

---
+++ src/player_interactions/ai_player.py
@@ -0,0 +1,43 @@
+from src.card import Card
+from src.hand import Hand
+from src.player import Player
+
+from src.player_interaction import PlayerInteraction
+
+
+class Bot(PlayerInteraction):
+    @classmethod
+    def choose_card(
+        cls, hand: Hand, top: Card, hand_counts: list[int] | None = None
+    ) -> Card:
+        """
+        Принимает решение, какую карту с руки играть.
+        Возвращает карту или None, если нельзя играть карту с руки.
+        """
+        playable_cards = [card for card in hand.cards if card.can_play_on(top)]
+        if playable_cards:
+            return playable_cards[0]
+        else:
+            return None
+
+    @classmethod
+    def choose_to_play(cls, top: Card, drawn: Card) -> bool:
+        """
+        Принимает решение играть или не играть взятую из колоды карту.
+        Бот всегда играет карту.
+        """
+        return True
+
+    @classmethod
+    def inform_card_drawn(cls, player: Player):
+        """
+        Сообщает, что игрок взял карту.
+        """
+        pass
+
+    @classmethod
+    def inform_card_played(cls, player: Player, card: Card):
+        """
+        Сообщает, что игрок сыграл карту.
+        """
+        pass
